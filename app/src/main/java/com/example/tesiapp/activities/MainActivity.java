d9840fc827b1c0a0256826e3fbecf3017466f15f
pisy1 <pisy1997@gmail.com>
2022-11-27 19:49:41 +0100

    Tesi App

--- app/src/main/java/com/example/tesiapp/activities/MainActivity.java
+++ app/src/main/java/com/example/tesiapp/activities/MainActivity.java
@@ -8,19 +8,28 @@ import android.util.Base64;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.tesiapp.R;
 import com.example.tesiapp.databinding.ActivityMainBinding;
 import com.example.tesiapp.utilities.Constants;
+import com.example.tesiapp.utilities.MyAdapter;
 import com.example.tesiapp.utilities.PreferenceManager;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.messaging.FirebaseMessaging;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    RecyclerView mRecyclerView;
+    List<ProductData> myProductList;
+    ProductData mProductData;
     private ActivityMainBinding binding;
     private PreferenceManager preferenceManager;
 
@@ -33,6 +42,29 @@ public class MainActivity extends AppCompatActivity {
         loadUserDetails();
         getToken();
         setListeners();
+
+        mRecyclerView = (RecyclerView)findViewById(R.id.recyclerView);
+
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(MainActivity.this,1);
+        mRecyclerView.setLayoutManager(gridLayoutManager);
+
+        myProductList = new ArrayList<>();
+
+        mProductData = new ProductData("Vino Nero Rossese","Vino rosso corposo ecc ecc ecc","8$",R.drawable.im1);
+        myProductList.add(mProductData);
+
+        mProductData = new ProductData("Vino Bianco Vermentino","Vino Bianco amabile ecc ecc ecc","8$",R.drawable.im2);
+        myProductList.add(mProductData);
+
+        mProductData = new ProductData("Olio oliva","Olio extra vergine oliva ecc ecc ecc","10$",R.drawable.im3);
+        myProductList.add(mProductData);
+
+        mProductData = new ProductData("olive in salamoia","olive in barattolo ecc ecc ecc","5$",R.drawable.im4);
+        myProductList.add(mProductData);
+
+        MyAdapter myAdapter = new MyAdapter(MainActivity.this,myProductList);
+        mRecyclerView.setAdapter(myAdapter);
+
     }
 
     private void setListeners() {
